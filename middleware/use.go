c2ad5808512667643e52ddc50c7bde23b34ff2fc
Marcelo Cardoso <marcelovicentegc@gmail.com>
2021-01-10 11:11:09 -0300

    chore: remove unused middleware

--- middleware/use.go
+++
@@ -1,9 +0,0 @@
-package middleware
-
-import "github.com/aws/aws-lambda-go/events"
-
-func UseMiddleware(f func(request events.APIGatewayCustomAuthorizerRequest)) func(events.APIGatewayCustomAuthorizerRequest) {
-	return func(request events.APIGatewayCustomAuthorizerRequest) {
-		f(request)
-	}
-}
