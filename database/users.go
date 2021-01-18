6a83eba9a0c1ee2164308fb3b740e3d71b041cfd
Marcelo Cardoso <marcelovicentegc@gmail.com>
2021-01-18 08:41:37 -0300

    chore: remove db error dump

--- database/users.go
+++ database/users.go
@@ -1,7 +1,5 @@
 package database
 
-import "fmt"
-
 func GetUser(email string) *User {
 	db := GetDB()
 
@@ -10,7 +8,6 @@ func GetUser(email string) *User {
 	result := db.Where("email = ?", email).First(&user)
 
 	if result.Error != nil {
-		fmt.Println("Something went wrong while getting a user by email: " + result.Error.Error())
 		return nil
 	}
 
